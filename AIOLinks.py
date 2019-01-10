cefbb1a20c1045d5cf5547b37fb7974a8688e7e4
Jacopo <ji_jacopo@yahoo.com>
2019-01-09 19:42:12 -0500

    Added Cadena job adv

--- AIOLinks.py
+++ AIOLinks.py
@@ -9,8 +9,8 @@ Mercedes        DONE
 Hayato          DONE
 Xenon           
 Phantom         
-Illium          
-Cadena          
+Illium          DONE(untested)
+Cadena          DONE(untested)
 Ark             
 Evan            
 '''
@@ -172,6 +172,7 @@ XenonJobs = [3600, 3610, 3611, 3612]
 PhantomJobs = [2400, 2410, 2411, 2412]
 EvanJobs = [2200, 2210, 2211, 2212, 2213, 2214, 2215, 2216, 2217, 2218]
 IlliumJobs = [15200,15210,15211,15212]
+CadenaJobs = [6400,6410,6411,6412]
 
 NpcTylusWarriorInstructor = 2020008
 NpcRobeiraMagicianInstructor = 2020009
@@ -262,6 +263,16 @@ def teleport_enter(x,y):
     time.sleep(1)
     Character.EnterPortal()
     time.sleep(1)
+
+def EnterPortal(name):
+    toggle_kami(False)
+    portal = Field.FindPortal(name)
+    pos = Character.GetPos()
+    if pos.x != portal.x:
+        Character.Teleport(portal.x, portal.y-20)
+    else:
+        Character.EnterPortal()
+        toggle_kami(True)
 
 def gotoGreatSpirit():
     while Field.GetID() != map2:
@@ -952,7 +963,416 @@ def IlliumSecond():
             print("Completed all Illium Quests and now returning control to rush by level")
             time.sleep(1)
             toggle_rush_by_level(True)
-############################################
+def CadenaFirst():
+    quest1 = Quest.GetQuestState(34600)
+    quest2 = Quest.GetQuestState(34601)
+    quest3 = Quest.GetQuestState(34602)
+    quest4 = Quest.GetQuestState(34603)
+    quest5 = Quest.GetQuestState(34656)
+    quest6 = Quest.GetQuestState(34604)
+    quest7 = Quest.GetQuestState(34605)
+    quest8 = Quest.GetQuestState(34606)
+    quest9 = Quest.GetQuestState(34607)
+    quest10 = Quest.GetQuestState(34608)
+    quest11 = Quest.GetQuestState(34609)
+    quest12 = Quest.GetQuestState(34610)
+    quest13 = Quest.GetQuestState(34611)
+    quest14 = Quest.GetQuestState(34612)
+    quest15 = Quest.GetQuestState(34613)
+    quest16 = Quest.GetQuestState(34614)
+    quest17 = Quest.GetQuestState(34615)
+    quest18 = Quest.GetQuestState(34616)
+    quest19 = Quest.GetQuestState(34617)
+    quest20 = Quest.GetQuestState(34618)
+    quest21 = Quest.GetQuestState(34619)
+    quest22 = Quest.GetQuestState(34620)
+    quest23 = Quest.GetQuestState(34621)
+    quest24 = Quest.GetQuestState(34622)
+    quest25 = Quest.GetQuestState(34623)
+    quest26 = Quest.GetQuestState(34624)
+    quest27 = Quest.GetQuestState(34625)
+    if quest1 != 2:
+        if quest1 == 0:
+            Quest.StartQuest(34600, 0)
+            time.sleep(10)
+        elif field_id == 940200500:
+            EnterPortal("west00")
+        elif field_id == 940200501:
+            EnterPortal("up00")
+    if quest2 != 2:
+        if quest2 == 0:
+            if field_id == 940200502:
+                Quest.StartQuest(34601, 3001221)
+            elif field_id == 940200600 or 940200601:
+                Key.Press(0x11)
+                time.sleep(1)
+        elif quest2 == 1:
+            Quest.CompleteQuest(34601, 3001221)
+    elif quest3 != 2:
+        if field_id == 402000002:
+            if quest3 == 0:
+                Quest.StartQuest(34602, 3001202)
+                time.sleep(10)
+            elif quest3 == 1:
+                Npc.ClearSelection()
+                Npc.RegisterSelection("remain")
+                Npc.RegisterSelection("Never")
+                Quest.CompleteQuest(34602, 3001202)
+                time.sleep(8)
+        else:
+            Terminal.Rush(402000002)
+    elif quest4 != 2:
+        if field_id == 402000002:
+            if quest4 == 0:
+                Quest.StartQuest(34603, 3001202)
+            elif quest4 == 1:
+                if Quest.CheckCompleteDemand(34603, 3001202) == 0:
+                    Quest.CompleteQuest(34603, 3001202)
+                else:
+                    Inventory.UseItem(2437264)
+        else:
+            Terminal.Rush(402000002)
+    elif quest5 != 2:
+        if field_id != 402000001:
+            Terminal.Rush(402000001)
+        else:
+            if quest5 == 0:
+                Quest.StartQuest(34656, 3001200)
+    elif quest6 != 2:
+        if field_id != 402000001:
+            Terminal.Rush(402000001)
+        else:
+            if quest6 == 0:
+                Quest.StartQuest(34604, 3001200)
+            elif quest6 == 1:
+                pos = Character.GetPos()
+                if pos.x != -308:
+                    toggle_kami(False)
+                    Character.Teleport(-308, 304)
+                else:
+                    Quest.CompleteQuest(34604, 3001210)
+                    toggle_kami(True)
+    elif quest7 != 2:
+        if quest7 == 0:
+            if field_id != 402000001:
+                Terminal.Rush(402000001)
+            else:
+                Quest.StartQuest(34605, 3001210)
+        elif quest7 == 1:
+            if Quest.CheckCompleteDemand(34605, 3001210) == 0:
+                if field_id != 402000001:
+                    Terminal.Rush(402000001)
+                else:
+                    Quest.CompleteQuest(34605, 3001210)
+            else:
+                if field_id != 402000110:
+                    Terminal.Rush(402000110)
+    elif quest8 != 2:
+        if quest8 == 0:
+            if field_id != 402000001:
+                Terminal.Rush(402000001)
+            else:
+                Quest.StartQuest(34606, 3001210)
+        elif quest8 == 1:
+            if Quest.CheckCompleteDemand(34606, 3001210) == 0:
+                if field_id != 402000001:
+                    Terminal.Rush(402000001)
+                else:
+                    Quest.CompleteQuest(34606, 3001210)
+            else:
+                if field_id != 402000111:
+                    Terminal.Rush(402000111)
+    elif quest9 != 2:
+        if quest9 == 0:
+            if field_id != 402000001:
+                Terminal.Rush(402000001)
+            else:
+                Quest.StartQuest(34607, 3001210)
+        elif quest9 == 1:
+            if Quest.CheckCompleteDemand(34607, 3001210) == 0:
+                if field_id != 402000001:
+                    Terminal.Rush(402000001)
+                else:
+                    Quest.CompleteQuest(34607, 3001210)
+            elif field_id != 402000112:
+                Terminal.Rush(402000112)
+    elif quest10 != 2:
+        if quest10 == 0:
+            if field_id != 402000001:
+                Terminal.Rush(402000001)
+            else:
+                pos = Character.GetPos()
+                if pos.x != -308:
+                    toggle_kami(False)
+                    Character.Teleport(-308, 304)
+                else:
+                    Quest.StartQuest(34608, 3001210)
+                    toggle_kami(True)
+        if quest10 == 1:
+            if field_id != 402000001:
+                Terminal.Rush(402000001)
+            else:
+                Quest.CompleteQuest(34608, 3001203)
+    elif quest11 != 2:
+        if quest11 == 0:
+            if field_id != 402000001:
+                Terminal.Rush(402000001)
+            else:
+                Quest.StartQuest(34609, 3001210)
+        elif quest11 == 1:
+            if Quest.CheckCompleteDemand(34609, 3001210) == 0:
+                if field_id != 402000001:
+                    Terminal.Rush(402000001)
+                else:
+                    Quest.CompleteQuest(34609, 3001210)
+            else:
+                if field_id != 402000220:
+                    Terminal.Rush(402000220)
+    elif quest12 != 2:
+        if quest12 == 0:
+            if field_id != 402000200:
+                Terminal.Rush(402000200)
+            else:
+                Quest.StartQuest(34610, 3001218)
+        elif quest12 == 1:
+            Quest.CompleteQuest(34610, 3001218)
+            Terminal.Sleep(3)
+    elif quest13 != 2:
+        if quest13 == 0:
+            if field_id == 940200700:
+                time.sleep(8)
+                EnterPortal("next00")
+                time.sleep(3)
+            elif field_id == 940200800:
+                time.sleep(8)
+                EnterPortal("next00")
+                time.sleep(3)
+            elif field_id == 940200900:
+                time.sleep(8)
+                EnterPortal("next00")
+                time.sleep(3)
+            else:
+                pos = Character.GetPos()
+                if pos.x != -506:
+                    toggle_kami(False)
+                    Character.Teleport(-506, 45)
+                    time.sleep(2)
+                else:
+                    Quest.StartQuest(34611, 3001218)
+                    toggle_kami(True)
+        elif quest13 == 1:
+            if field_id == 940200900:
+                EnterPortal("next00")
+                time.sleep(5)
+            if field_id == 402000210:
+                Quest.CompleteQuest(34611, 3001214)
+    elif quest14 != 2:
+        if quest14 == 0:
+            Quest.StartQuest(34612, 3001214)
+        elif quest14 == 1:
+            Quest.CompleteQuest(34612, 3001214)
+            time.sleep(2)
+    elif quest15 != 2:
+        if quest15 == 0:
+            if field_id != 402000001:
+                Terminal.Rush(402000001)
+            else:
+                time.sleep(3)
+                Quest.StartQuest(34613, 0)
+        elif quest15 == 1:
+            if field_id != 402000001:
+                Terminal.Rush(402000001)
+            else:
+                Quest.CompleteQuest(34613, 3001210)
+                time.sleep(2)
+    elif quest16 != 2:
+        if quest16 == 0:
+            Quest.StartQuest(34614, 0)
+        elif quest16 == 1:
+            if field_id != 402000001:
+                Terminal.Rush(402000001)
+            else:
+                pos = Character.GetPos()
+                if pos.x != -98:
+                    toggle_kami(False)
+                    Character.Teleport(-98, 304)
+                else:
+                    Quest.CompleteQuest(34614, 3001226)
+                    toggle_kami(True)
+    elif quest17 != 2:
+        if quest17 == 0:
+            Quest.StartQuest(34615, 0)
+            time.sleep(3)
+        elif quest17 == 1:
+            # Talk to Gusto
+            Terminal.Rush(402000200)
+            while Terminal.IsRushing():
+                time.sleep(5)
+            Character.TalkToNpc(3001218)
+            time.sleep(3)
+
+            # Talk to Corbo
+            Terminal.Rush(402000100)
+            while Terminal.IsRushing():
+                time.sleep(5)
+            Character.TalkToNpc(3001219)
+            time.sleep(3)
+
+            # Talk to Antuin
+            Terminal.Rush(402000000)
+            while Terminal.IsRushing():
+                time.sleep(5)
+            pos = Character.GetPos()
+            if pos.x != -1639:
+                toggle_kami(False)
+                Character.Teleport(-1639, 35-20)
+                time.sleep(3)
+            Character.TalkToNpc(3001212)
+            time.sleep(8)
+            toggle_kami(True)
+    elif quest18 != 2:
+        if quest18 == 0:
+            if field_id != 402000000:
+                Terminal.Rush(402000000)
+            else:
+                Quest.StartQuest(34616, 0)
+        elif quest18 == 1:
+            if field_id != 402000000:
+                Terminal.Rush(402000000)
+            else:
+                Quest.CompleteQuest(34616, 3001204)
+    elif quest19 != 2:
+        if quest19 == 0:
+            if field_id != 402000000:
+                Terminal.Rush(402000000)
+            else:
+                Quest.StartQuest(34617, 3001204)
+        elif quest19 == 1:
+            if Quest.CheckCompleteDemand(34617, 3001204) == 0:
+                if field_id != 402000000:
+                    Terminal.Rush(402000000)
+                else:
+                    pos = Character.GetPos()
+                    if pos.x != -1639:
+                        toggle_kami(False)
+                        Character.Teleport(-1639, 35 - 20)
+                    else:
+                        Quest.CompleteQuest(34617, 3001204)
+                        toggle_kami(True)
+            else:
+                if field_id != 402000120:
+                    Terminal.Rush(402000120)
+    elif quest20 != 2:
+        if quest20 == 0:
+            if field_id != 402000000:
+                Terminal.Rush(402000000)
+            else:
+                Quest.StartQuest(34618, 3001204)
+        elif quest20 == 1:
+            if Quest.CheckCompleteDemand(34618, 3001204) == 0:
+                if field_id != 402000000:
+                    Terminal.Rush(402000000)
+                else:
+                    pos = Character.GetPos()
+                    if pos.x != -1639:
+                        toggle_kami(False)
+                        Character.Teleport(-1639, 35 - 20)
+                    else:
+                        Quest.CompleteQuest(34618, 3001204)
+                        toggle_kami(True)
+            else:
+                if field_id != 402000121:
+                    Terminal.Rush(402000121)
+    elif quest21 != 2:
+        if quest21 == 0:
+            while Field.GetID() == 940200507:
+                Key.Press(0x20)
+                Key.Up(0x20)
+                time.sleep(0.1)
+            else:
+                pos = Character.GetPos()
+                if pos.x != -1701:
+                    toggle_kami(False)
+                    Character.Teleport(-1701, 27 - 20)
+                    time.sleep(3)
+                Quest.StartQuest(34619, 3001204)
+                toggle_kami(True)
+    elif quest22 != 2:
+        if quest22 == 0:
+            if field_id != 402000000:
+                Terminal.Rush(402000000)
+            else:
+                Quest.StartQuest(34620, 3001212)
+        elif quest22 == 1:
+            if field_id != 402000000:
+                Terminal.Rush(402000000)
+            else:
+                Quest.CompleteQuest(34620, 3001212)
+    elif quest23 != 2:
+        if quest23 == 0:
+            if field_id != 402000000:
+                Terminal.Rush(402000000)
+            else:
+                Quest.StartQuest(34621, 0)
+        elif quest23 == 1:
+            if Quest.CheckCompleteDemand(34621, 3001228) == 0:
+                if field_id != 402000000:
+                    Terminal.Rush(402000000)
+                else:
+                    Quest.CompleteQuest(34621, 3001228)
+            else:
+                if field_id != 402000122:
+                    Terminal.Rush(402000122)
+    elif quest24 != 2:
+        if quest24 == 0:
+            if field_id != 402000000:
+                Terminal.Rush(402000000)
+            else:
+                Quest.StartQuest(34622, 0)
+        elif quest24 == 1:
+            if field_id != 402000301:
+                Terminal.Rush(402000301)
+            else:
+                Quest.CompleteQuest(34622, 3001220)
+    elif quest25 != 2:
+        if quest25 == 0:
+            if field_id == 940201000:
+                time.sleep(1)
+            elif field_id != 402000301:
+                Terminal.Rush(402000301)
+            else:
+                Quest.StartQuest(34623, 3001223)
+        elif quest25 == 1:
+            if field_id == 402000301:
+                Quest.CompleteQuest(34623, 3001211)
+    elif quest26 != 2:
+        if quest26 == 0:
+            if field_id != 402000001:
+                Terminal.Rush(402000001)
+            else:
+                Quest.StartQuest(34624, 3001200)
+        elif quest26 == 1:
+            if field_id != 402000001:
+                Terminal.Rush(402000001)
+            else:
+                Quest.CompleteQuest(34624, 3001200)
+    elif quest27 != 2:
+        if quest27 == 0:
+            if field_id != 402000001:
+                Terminal.Rush(402000001)
+            else:
+                Quest.StartQuest(34625, 3001200)
+        elif quest27 == 1:
+            if field_id != 402000400:
+                Terminal.Rush(402000400)
+            else:
+                Quest.CompleteQuest(34625, 3001205)
+                
+    if job == 6400 and level >= 30:
+        Quest.StartQuest(34657, 3001250)
+        toggle_kami(True)
+        toggle_rush_by_level(True)
+################################################################
 def id2str(jobid):
     if jobid in LuminousJobs:
         return "Luminous"
@@ -1699,6 +2119,22 @@ def toggleAttack(on):
         Terminal.SetCheckBox("Auto Attack",False)
         Terminal.SetCheckBox("bot/illium/radiant_javelin_delay",True)
         Terminal.SetCheckBox("General FMA",True)
+    elif job == 6400 or job == 6410 or job == 6411: #Cadena 1st + 2nd + 3rd 64001006 or 64001001
+        Key.Set(pgup_key, 2, 2001582)
+        Terminal.SetLineEdit("SISkillID","64001006")
+        Terminal.SetSpinBox("SkillInjection",150)
+        Terminal.SetRadioButton("SIRadioCadena",True)
+        Terminal.SetCheckBox("Melee No Delay",True)
+        Terminal.SetCheckBox("Auto Attack",False)
+        Terminal.SetCheckBox("Skill Injection", on)
+    elif job == 6412: # Cadena 4th job 64121016 May want to change this 
+        Key.Set(pgup_key, 2, 2001582)
+        Terminal.SetLineEdit("SISkillID","64121016")
+        Terminal.SetSpinBox("SkillInjection",150)
+        Terminal.SetRadioButton("SIRadioCadena",True)
+        Terminal.SetCheckBox("Melee No Delay",True)
+        Terminal.SetCheckBox("Auto Attack",False)
+        Terminal.SetCheckBox("Skill Injection", on)
     elif job == 6500: #AB 1st
         Terminal.SetLineEdit("SISkillID","65001100")
         Terminal.SetCheckBox("Auto Attack", False)
@@ -1928,7 +2364,9 @@ elif job == 15200:
 elif job == 15210:
     print("Completing Illium Second Job")
     IlliumSecond()
-    
+elif job == 6400:
+    print("Completing Cadena First Job and Second Job")
+    CadenaFirst()
 ###### lvl 50 hyper rock #######
 if Quest.GetQuestState(61589) !=2 and Character.GetLevel() >= 50:
     #print("Getting hyper rock")
@@ -2009,6 +2447,9 @@ if Character.GetLevel() >= 13 and GameState.IsInGame():
     if job in IlliumJobs:
         lookback = Quest.GetQuestState(34820)
         if lookback == 2:
+            getBoogie()
+    elif job in CadenaJobs:
+        if job != 6400:
             getBoogie()
     else:
         getBoogie()
