cc1577bd964b6fdf1c4358f4af24a068e622f025
Jacopo <ji_jacopo@yahoo.com>
2019-01-09 22:42:07 -0500

    Ark done

--- AIOLinks.py
+++ AIOLinks.py
@@ -11,7 +11,7 @@ Xenon
 Phantom         
 Illium          DONE(untested)
 Cadena          DONE(untested)
-Ark             
+Ark             DONE(untested)
 Evan            
 '''
 curbrockhideout = [600050000,600050010,600050020]
@@ -265,11 +265,12 @@ def teleport_enter(x,y):
     time.sleep(1)
 
 def EnterPortal(name):
-    toggle_kami(False)
     portal = Field.FindPortal(name)
     pos = Character.GetPos()
     if pos.x != portal.x:
+        toggle_kami(False)
         Character.Teleport(portal.x, portal.y-20)
+        time.sleep(1)
     else:
         Character.EnterPortal()
         toggle_kami(True)
@@ -282,6 +283,50 @@ def gotoGreatSpirit():
         if Field.GetID() == map1:
             Character.TalkToNpc(1033211)
             time.sleep(1)
+
+def StartQuest(quest, npc, map):
+   if Terminal.IsRushing():
+       time.sleep(1)
+   elif mapID != map:
+       time.sleep(3)
+       Terminal.Rush(map)
+   else:
+       location = Field.FindNpc(npc) #change
+       if location.valid:
+           if abs(Character.GetPos().x - location.x) > 400:
+               toggle_kami(False)
+               Character.Teleport(location.x, location.y)
+       time.sleep(1)
+       Quest.StartQuest(quest, npc)
+       time.sleep(1)
+       toggle_kami(True)
+
+def CompleteQuest(quest, npc, map):
+   if Terminal.IsRushing():
+       time.sleep(1)
+   elif mapID != map:
+       time.sleep(3)
+       Terminal.Rush(map)
+   else:
+       location = Field.FindNpc(npc)
+       if location.valid:
+           if abs(Character.GetPos().x - location.x) > 400:
+               toggle_kami(False)
+               Character.Teleport(location.x, location.y)
+       time.sleep(1)
+       Quest.CompleteQuest(quest, npc)
+       time.sleep(1)
+       toggle_kami(True)
+
+def KillMobAndLoot(map):
+   if Terminal.IsRushing():
+       time.sleep(1)
+   elif mapID != map:
+       Terminal.Rush(map)
+       time.sleep(3)
+   else:
+       Terminal.SetCheckBox("Kami Loot", True)
+       Terminal.SetCheckBox("Auto Loot", True)
 
 #########Job specific advancements##########
 def kannaFirst():
@@ -1372,6 +1417,330 @@ def CadenaFirst():
         Quest.StartQuest(34657, 3001250)
         toggle_kami(True)
         toggle_rush_by_level(True)
+
+def ArkFirst():
+    quest1 = Quest.GetQuestState(34915)
+    quest2 = Quest.GetQuestState(34916)
+    quest3 = Quest.GetQuestState(34917)
+    quest4 = Quest.GetQuestState(34918)
+    quest5 = Quest.GetQuestState(34919)
+    quest6 = Quest.GetQuestState(34920)
+    quest7 = Quest.GetQuestState(34921)
+    quest8 = Quest.GetQuestState(34922)
+    quest9 = Quest.GetQuestState(34923)
+    quest10 = Quest.GetQuestState(34924)
+    quest11 = Quest.GetQuestState(34925)
+    quest12 = Quest.GetQuestState(34926)
+    quest13 = Quest.GetQuestState(34927)
+    quest14 = Quest.GetQuestState(34928)
+    quest15 = Quest.GetQuestState(34929)
+    quest16 = Quest.GetQuestState(34930)
+    quest17 = Quest.GetQuestState(34931)
+    quest18 = Quest.GetQuestState(34932)
+    quest19 = Quest.GetQuestState(34933)
+    quest20 = Quest.GetQuestState(34934)
+    quest21 = Quest.GetQuestState(34935)
+    quest22 = Quest.GetQuestState(34936)
+    quest23 = Quest.GetQuestState(34937)
+    quest24 = Quest.GetQuestState(34938)
+    quest25 = Quest.GetQuestState(34902)
+    if quest1 != 2:
+        print("1")
+        if quest1 == 0:
+            StartQuest(34915, 3001406, 402000615)
+        if field_id == 940205000:
+            toggle_kami(True)
+    elif quest2 != 2:   
+        print("2")
+        if quest2 == 0:
+            toggle_kami(False)
+            StartQuest(34916, 3001400, 402000600)
+        elif quest2 == 1:
+            toggle_kami(False)
+            CompleteQuest(34916, 3001400, 402000600)
+    elif quest3 != 2:
+        print("3")
+        if quest3 == 0:
+            toggle_kami(False)
+            StartQuest(34917, 3001400, 402000600)
+        elif quest3 == 1:
+            if Quest.CheckCompleteDemand(34917, 3001400):
+                KillMobAndLoot(402000610)
+                time.sleep(5)
+            else:
+                toggle_kami(False)
+                CompleteQuest(34917, 3001400, 402000600)
+    elif quest4 != 2:
+        print("4")
+        if quest4 == 0:
+            toggle_kami(False)
+            StartQuest(34918, 3001400, 402000600)
+        elif quest4 == 1:
+            toggle_kami(False)
+            CompleteQuest(34918, 3001401, 402000600)
+    elif quest5 != 2:
+        print("5")
+        if quest5 == 0:
+            toggle_kami(False)
+            StartQuest(34919, 3001401, 402000600)
+        elif quest5 == 1:
+            if Quest.CheckCompleteDemand(34919, 3001401):
+                KillMobAndLoot(402000611)
+                time.sleep(5)
+            else:
+                toggle_kami(False)
+                CompleteQuest(34919, 3001401, 402000600)
+    elif quest6 != 2:
+        print("6")
+        if quest6 == 0:
+            toggle_kami(False)
+            StartQuest(34920, 3001401, 402000600)
+        elif quest6 == 1:
+            CompleteQuest(34920, 3001402, 402000600)
+    elif quest7 != 2:
+        print("7")
+        if quest7 == 0:
+            toggle_kami(False)
+            StartQuest(34921, 3001402, 402000600)
+        elif quest7 == 1:
+            if Quest.CheckCompleteDemand(34921, 3001402):
+                KillMobAndLoot(402000612)
+                time.sleep(5)
+            else:
+                toggle_kami(False)
+                CompleteQuest(34921, 3001402, 402000600)
+    elif quest8 != 2:
+        print("8")
+        if quest8 == 0:
+            toggle_kami(False)
+            StartQuest(34922, 3001402, 402000600)
+        elif quest8 == 1:
+            toggle_kami(False)
+            CompleteQuest(34922, 3001403, 402000600)
+    elif quest9 != 2:
+        print("9")
+        if quest9 == 0:
+            toggle_kami(False)
+            StartQuest(34923, 3001404, 402000613)
+        elif quest9 == 1:
+            if Quest.CheckCompleteDemand(34923, 3001404):
+                KillMobAndLoot(402000613)
+                time.sleep(5)
+            else:
+                toggle_kami(False)
+                CompleteQuest(34923, 3001404, 402000613)
+    elif quest10 != 2:
+        print("10")
+        if quest10 == 0:
+            toggle_kami(False)
+            StartQuest(34924, 3001400, 402000600)
+        elif quest10 == 1:
+            toggle_kami(False)
+            CompleteQuest(34924, 3001405, 402000614)
+    elif quest11 != 2:
+        print("11")
+        if quest11 == 0:
+            toggle_kami(False)
+            StartQuest(34925, 3001405, 402000614)
+        elif quest11 == 1:
+            toggle_kami(False)
+            CompleteQuest(34925, 3001400, 402000600)
+    elif quest12 != 2:
+        print("12")
+        if quest12 == 0:
+            toggle_kami(False)
+            StartQuest(34926, 3001402, 402000600)
+        elif quest12 == 1:
+            if Quest.CheckCompleteDemand(34926, 3001402):
+                KillMobAndLoot(402000616)
+                time.sleep(5)
+            else:
+                toggle_kami(False)
+                CompleteQuest(34926, 3001402, 402000600)
+    elif quest13 != 2:
+        print("13")
+        if quest13 == 0:
+            toggle_kami(False)
+            StartQuest(34927, 3001401, 402000600)
+        elif quest13 == 1:
+            if Quest.CheckCompleteDemand(34927, 3001401):
+                KillMobAndLoot(402000617)
+                time.sleep(5)
+            else:
+                toggle_kami(False)
+                CompleteQuest(34927, 3001401, 402000600)
+    elif quest14 != 2:
+        print("14")
+        if quest14 == 0:
+            toggle_kami(False)
+            StartQuest(34928, 3001400, 402000600)
+        elif quest14 == 1:
+            toggle_kami(False)
+            CompleteQuest(34928, 3001407, 402000615)
+    elif quest15 != 2:
+        print("15")
+        if quest15 == 0:
+            toggle_kami(False)
+            StartQuest(34929, 3001400, 402000600)       
+        elif quest15 == 1:
+            toggle_kami(False)
+            CompleteQuest(34929, 3001408, 402000620)
+    elif quest16 != 2:
+        print("16")
+        if quest16 == 0:
+            toggle_kami(False)
+            StartQuest(34930, 3001409, 402000621)
+        elif quest16 == 1:
+            if Quest.CheckCompleteDemand(34930, 3001409):
+                KillMobAndLoot(402000621)
+                time.sleep(5)
+            else:
+                toggle_kami(False)
+                CompleteQuest(34930, 3001409, 402000621)
+    elif quest17 != 2:
+        print("17")
+        if quest17 == 0:
+            toggle_kami(False)
+            StartQuest(34931, 3001410, 402000622)
+        elif quest17 == 1:
+            if Quest.CheckCompleteDemand(34931, 3001410):
+                KillMobAndLoot(402000622)
+                time.sleep(5)
+            else:
+                toggle_kami(False)
+                CompleteQuest(34931, 3001410, 402000622)
+    elif quest18 != 2:
+        print("18")
+        if quest18 == 0:
+            toggle_kami(False)
+            StartQuest(34932, 3001411, 402000630)
+        elif quest18 == 1:
+            toggle_kami(False)
+            CompleteQuest(34932, 3001412, 402000631)
+    elif quest19 != 2:
+        print("19")
+        if quest19 == 0:
+            toggle_kami(False)
+            StartQuest(34933, 3001412, 402000631)
+        elif quest19 == 1:
+            if Quest.CheckCompleteDemand(34933, 3001412):
+                KillMobAndLoot(402000631)
+                time.sleep(5)
+            else:
+                toggle_kami(False)
+                CompleteQuest(34933, 3001412, 402000631)
+    elif quest20 != 2:
+        print("20")
+        if quest20 == 0:
+            toggle_kami(False)
+            StartQuest(34934, 3001413, 402000633)
+        elif quest20 == 1:
+            if Quest.CheckCompleteDemand(34934, 3001413):
+                KillMobAndLoot(402000633)
+                time.sleep(5)
+            else:
+                toggle_kami(False)
+                CompleteQuest(34934, 3001413, 402000633)
+    elif quest21 != 2:
+        print("21")
+        if quest21 == 0:
+            toggle_kami(False)
+            StartQuest(34935, 3001414, 402000635)
+        elif quest21 == 1:
+            toggle_kami(False)
+            CompleteQuest(34935, 3001416, 402000648)
+    elif quest22 != 2:
+        print("22")
+        if quest22 == 0:
+            toggle_kami(False)
+            StartQuest(34936, 3001415, 402000648)
+            time.sleep(2)
+            while Field.GetID() == 402090006:
+                Key.Press(0x20)
+                Key.Press(0x88)
+                print("press")
+    elif quest23 != 2:
+        print("23")
+        if quest23 == 0:
+            toggle_kami(False)
+            StartQuest(34937, 3001417, 402000644)
+        elif quest23 == 1:
+            toggle_kami(False)
+            CompleteQuest(34937, 3001417, 402000644)
+    elif quest24 != 2:
+        print("24")
+        if quest24 == 0:
+            if Field.GetID() == 402000644:
+                toggle_kami(False)
+                StartQuest(34938, 3001423, 402000644)
+            elif Field.GetID() == 940205100:
+                toggle_kami(True)
+                time.sleep(3)
+                EnterPortal("next00")
+            elif Field.GetID() == 940205200:
+                toggle_kami(True)
+                time.sleep(3)
+                EnterPortal("next00")
+            elif Field.GetID() == 940205300:
+                toggle_kami(True)
+                time.sleep(3)
+                EnterPortal("next00")
+    elif quest25 != 2:
+        print("25")
+        if quest25 == 0:
+            toggle_kami(False)
+            StartQuest(34902, 0, 402000640)
+
+def ArkSecond():
+    quest1 = Quest.GetQuestState(34939)
+    quest2 = Quest.GetQuestState(34940)
+    if quest1 != 2:
+        if quest1 == 0:
+            toggle_kami(False)
+            StartQuest(34939, 0, 402000640)
+        elif quest1 == 1:
+            toggle_kami(False)
+            CompleteQuest(34939, 0, 402000640)
+    elif quest2 != 2:
+        if quest2 == 0:
+            if Field.GetID() == 402000640:
+                toggle_kami(False)
+                StartQuest(34940, 0, 402000640)
+        elif Field.GetID() == 940205400:
+            toggle_kami(True)
+            time.sleep(3)
+            TakePortal("next00")
+        elif Field.GetID() == 940205500:
+            toggle_kami(True)
+            time.sleep(3)
+            TakePortal("next00")
+        elif Field.GetID() == 940205600:
+            toggle_kami(True)
+            time.sleep(3)
+            TakePortal("next00")
+        elif Field.GetID() == 940205900:
+            toggle_kami(True)
+            time.sleep(3)
+    elif level >= 60:
+        jobQuest = Quest.GetQuestState(34903)
+        print("Completing Ark Third Job Adv")
+        if jobQuest != 2:
+            if jobQuest == 0:
+                Quest.StartQuest(34903, 0)
+                time.sleep(5)
+            elif jobQuest == 1:
+                Quest.CompleteQuest(34903, 0)
+                time.sleep(5)
+
+def ArkFourth():
+    jobQuest = Quest.GetQuestState(34904)
+    if jobQuest != 2:
+        if jobQuest == 0:
+            Quest.StartQuest(34904, 0)
+            time.sleep(5)
+        elif jobQuest == 1:
+            Quest.CompleteQuest(34904, 0)
+            time.sleep(5)
 ################################################################
 def id2str(jobid):
     if jobid in LuminousJobs:
@@ -1785,6 +2154,10 @@ def safety_setting():
 def toggleAttack(on):
     attack_key = 0x44
     pgup_key = 0x21
+    if Character.IsOwnFamiliar(9960098):
+        Terminal.SetSlider("sliderMP", 90)
+    else:
+        Terminal.SetSlider("sliderMP", 10)
     if job == 3712:
         Terminal.SetLineEdit("SISkillID","37121003")
         Terminal.SetCheckBox("Auto Attack", False)
@@ -2136,13 +2509,18 @@ def toggleAttack(on):
         Terminal.SetCheckBox("Auto Attack",False)
         Terminal.SetCheckBox("Skill Injection", on)
     elif job == 15500 or job == 15510 or job == 15511: #Ark 1st + 2nd + 3rd 155001100
-        Terminal.SetCheckBox("Skill Injection", True)
-        Terminal.SetCheckBox("General FMA", False)
+        Key.Set(pgup_key, 2, 2001582)
         Terminal.SetLineEdit("SISkillID", "155001100")
+        Terminal.SetCheckBox("Skill Injection", True)
+        Terminal.SetCheckBox("Melee No Delay",True)
+        Terminal.SetSpinBox("SkillInjection",700)
         Terminal.SetRadioButton("SIRadioMelee",True)
-        Terminal.SetSlider("sliderMP", 10)
-        Terminal.SetSpinBox("KamiLoot", 10)
-        Key.Set(0x22, 2, 2001506)
+    elif job == 15512: #Ark 4th 155121007 @50
+        Terminal.SetLineEdit("SISkillID", "155121007")
+        Terminal.SetCheckBox("Skill Injection", True)
+        Terminal.SetCheckBox("Melee No Delay",True)
+        Terminal.SetSpinBox("SkillInjection",50)
+        Terminal.SetRadioButton("SIRadioMelee",True)
     elif job == 6500: #AB 1st
         Terminal.SetLineEdit("SISkillID","65001100")
         Terminal.SetCheckBox("Auto Attack", False)
@@ -2375,6 +2753,17 @@ elif job == 15210:
 elif job == 6400:
     print("Completing Cadena First Job and Second Job")
     CadenaFirst()
+elif job == :
+    print("Starting Ark job")
+elif job == 15500:
+    print("Completing Ark First Job")
+    ArkFirst()
+elif job == 15510:
+    print("Completing Ark Second Job")
+    ArkSecond()
+elif job == 15511 and level >= 100:
+    print("Completing Ark Fourth Job")
+    ArkFourth()
 ###### lvl 50 hyper rock #######
 if Quest.GetQuestState(61589) !=2 and Character.GetLevel() >= 50:
     #print("Getting hyper rock")
@@ -2458,6 +2847,9 @@ if Character.GetLevel() >= 13 and GameState.IsInGame():
             getBoogie()
     elif job in CadenaJobs:
         if job != 6400:
+            getBoogie()
+    elif job in ArkJobs:
+        if job == 15511:
             getBoogie()
     else:
         getBoogie()
